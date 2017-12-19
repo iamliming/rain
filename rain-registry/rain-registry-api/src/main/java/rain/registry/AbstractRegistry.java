f7968ca5f7630cac4ae2b8e3d259ed6d5e1d31fa
LM <greenwater007@163.com>
2017-12-19 14:10:06 +0800

    comment api

--- rain-registry/rain-registry-api/src/main/java/rain/registry/AbstractRegistry.java
+++ rain-registry/rain-registry-api/src/main/java/rain/registry/AbstractRegistry.java
@@ -1,6 +1,10 @@
 package rain.registry;
 
+import java.util.Set;
+import java.util.concurrent.CopyOnWriteArraySet;
 import java.util.concurrent.atomic.AtomicBoolean;
+
+import rain.common.URL;
 
 /**
  * <一句话功能简述> <功能详细描述>
@@ -15,6 +19,46 @@ public abstract class AbstractRegistry implements Registry
     // 关闭
     AtomicBoolean isDestryed = new AtomicBoolean(false);
 
+    private Set<URL> registred = new CopyOnWriteArraySet<URL>();
+
+
+    public URL url()
+    {
+        return null;
+    }
+
+    public void subscribe(URL url, NotifyListener listener)
+    {
+        doSubscribe(url, listener);
+    }
+
+    public void unsubscribe(URL url, NotifyListener listener)
+    {
+        doUnsubscribe(url, listener);
+    }
+
+    protected abstract void doUnsubscribe(URL url, NotifyListener listener);
+
+    public void registry(URL url)
+    {
+        doRegistry(url);
+    }
+
+    public void unRegistry(URL url)
+    {
+        doUnRegistry(url);
+    }
+
+    protected abstract void doRegistry(URL url);
+
+    protected abstract void doUnRegistry(URL url);
+
+    protected abstract void doSubscribe(URL url, NotifyListener listener);
+
+    public boolean abviable(URL url)
+    {
+        return false;
+    }
 
     public void destry()
     {
